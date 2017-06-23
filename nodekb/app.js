2b62f17b3ec4bbf87a3c1c2b9929bc49eb3c5732
vadimsoltan <soltan.vadim@gmail.com>
2017-06-23 10:04:29 -0400

    Added nodekb

---
+++ nodekb/app.js
@@ -0,0 +1,162 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// check connection
+db.once('open', function(){
+  console.log('Connected to MongoDB');
+});
+
+// check for db errors
+db.on('error', function(err){
+  console.log(err);
+});
+
+// Init App
+const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
+
+//Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// body-parser middlewear parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Home Route
+app.get('/', function(req, res){
+  Article.find({}, function(err, articles) {
+    if(err){
+      console.log(err)
+    } else {
+      res.render('index', {
+        title:'Articles',
+        articles: articles
+      });
+    }
+  });
+});
+
+// Add Route
+app.get('/articles/add', function(req, res){
+  res.render('add_article', {
+    title:'Add Articles'
+  });
+});
+
+// Load Edit Form
+app.get('/article/edit/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('edit_article', {
+      title: 'Edit Article',
+      article:article
+    });
+  });
+});
+
+// Add Submit POST Route
+app.post('/articles/add', function(req, res) {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err){
+    if(err) {
+      conole.log(err);
+      return;
+    } else {
+      req.flash('success', 'Article Added');
+      res.redirect('/');
+    }
+  });
+});
+
+// Update Submit
+app.post('/articles/edit/:id', function(req, res) {
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article, function(err){
+    if(err) {
+      conole.log(err);
+      return;
+    } else {
+      req.flash('success', 'Article Updated');
+      res.redirect('/');
+    }
+  });
+});
+
+// Get a single article
+app.get('/article/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('article', {
+      article:article
+    });
+  });
+});
+
+app.delete('/article/:id', function(req, res){
+  let query = {_id:req.params.id}
+  Article.remove(query, function(err){
+    if(err){
+      console.log(err);
+    }
+    res.send('Success');
+  });
+});
+// Start server
+app.listen(3000, function(){
+  console.log('Server started on port 3000...')
+});
