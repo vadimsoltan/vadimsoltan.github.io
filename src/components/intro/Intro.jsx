5a81fac015b8fca4fe8331f6a6f28e28ce5d5f8c
vadimsoltan <vadimsoltan@Oban.localdomain>
2022-09-10 22:12:49 -0400

    presentable version2

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,39 @@
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Software", "Data", "UX/UI"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Vadim Soltan.</h1>
+          <h3>
+            <span ref={textRef}></span> Engineer
+          </h3>
+        </div>
+        <a href="#resume">
+          <ExpandMoreIcon className="icon" />
+        </a>
+      </div>
+    </div>
+  );
+}
